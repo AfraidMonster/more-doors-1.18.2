4d36becf7b07f06cea04d6215ad56dad567ba157
AfraidMonster <henrygfraser@icloud.com>
2022-05-12 22:46:11 +0100

    More Doors

---
+++ src/main/java/afraidmonster/moredoors/CustomDoorBlock.java
@@ -0,0 +1,16 @@
+package afraidmonster.moredoors;
+
+import net.minecraft.block.DoorBlock;
+
+public class CustomDoorBlock extends DoorBlock {
+
+    protected CustomDoorBlock(Settings settings) {
+        super(settings.nonOpaque());
+        //TODO Auto-generated constructor stub
+
+        
+    }
+
+    
+
+}
---
+++ src/main/java/afraidmonster/moredoors/CustomTrapdoorBlock.java
@@ -0,0 +1,12 @@
+package afraidmonster.moredoors;
+
+import net.minecraft.block.TrapdoorBlock;
+
+public class CustomTrapdoorBlock extends TrapdoorBlock{
+
+    protected CustomTrapdoorBlock(Settings settings) {
+        super(settings.nonOpaque());
+        //TODO Auto-generated constructor stub
+    }
+    
+}
